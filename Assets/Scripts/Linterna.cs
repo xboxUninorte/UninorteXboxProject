8e4de57c9e8d32f3eb97e8597591ea71a452fbf4
ErwinALX <erwinb1996@outlook.com>
2015-11-13 11:57:17 -0500

    player incluido + linterna

---
+++ Assets/Scripts/Linterna.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Linterna : MonoBehaviour {
+
+	// Use this for initialization
+	public Light luz;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			if(luz.GetComponent<Light>().enabled == true){
+				luz.GetComponent<Light>().enabled = false;
+				Debug.Log ("apagada");
+			} else {
+				luz.GetComponent<Light>().enabled = true;
+				Debug.Log ("encendida");
+			}
+		}
+	}
+}
